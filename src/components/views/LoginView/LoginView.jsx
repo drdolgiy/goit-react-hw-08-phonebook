1761589891dc37ff274f79a721dfadc9a41f4df8
Alexey Dolgiy <alexey.dolgiy@gmail.com>
2022-05-10 17:16:15 +0300

    update components

--- src/components/views/LoginView/LoginView.jsx
+++ src/components/views/LoginView/LoginView.jsx
@@ -1,8 +1,8 @@
-import { useState } from "react";
+// import { useState } from "react";
 
-export const Login = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+export const LoginView = () => {
+    // const [email, setEmail] = useState('');
+    // const [password, setPassword] = useState('');
 
     return (
         <form>
