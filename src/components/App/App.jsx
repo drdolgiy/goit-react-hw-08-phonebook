89e718a2739cd541b34ce414151d4700a53fa472
Alexey Dolgiy <alexey.dolgiy@gmail.com>
2022-05-10 17:23:18 +0300

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,5 @@
-import { useSelector,useDispatch } from 'react-redux';
-import { useFetchContactsQuery, useDeleteContactMutation } from 'redux/contactsApi';
+// import { useSelector,useDispatch } from 'react-redux';
+// import { useFetchContactsQuery, useDeleteContactMutation } from 'redux/contactsApi';
 import { Suspense, Routes, Route } from 'react-router-dom';
 import {AppBar} from '../AppBar/AppBar'
 import {LoginView} from '../views/LoginView/LoginView'
@@ -9,26 +9,26 @@ import { RegisterView } from '../views/RegisterView/RegisterView'
 // import { Form } from '../Form/Form';
 // import {Filter} from '../Filter/Filter';
 // import {ContactList} from '../ContactList/ContactList';
-// import {Container} from './App.styled'
+import {Container} from './App.styled'
 // import { filteredContacts } from 'redux/filterSlice';
 // import { Rings } from 'react-loader-spinner';
 
-// export default function App() {
-//     const { data, isFetching } = useFetchContactsQuery();
-//     const [deleteContact] = useDeleteContactMutation();
-//     const dispatch = useDispatch();
-//     const filter = useSelector(state => state.filter.value);    
+export default function App() {
+    // const { data, isFetching } = useFetchContactsQuery();
+    // const [deleteContact] = useDeleteContactMutation();
+    // const dispatch = useDispatch();
+    // const filter = useSelector(state => state.filter.value);    
 
-//     const filteredContact = () => {
-//         const normalizedFilter = filter.toLowerCase();
-//         return data.filter(contact =>
-//              (contact.name.toLowerCase().includes(normalizedFilter)));
-//     };
+    // const filteredContact = () => {
+    //     const normalizedFilter = filter.toLowerCase();
+    //     return data.filter(contact =>
+    //          (contact.name.toLowerCase().includes(normalizedFilter)));
+    // };
 
-//     const changeFilter = event => {
-//         const inputValue = event.currentTarget.value;
-//         dispatch(filteredContacts(inputValue));
-//     };
+    // const changeFilter = event => {
+    //     const inputValue = event.currentTarget.value;
+    //     dispatch(filteredContacts(inputValue));
+    // };
 
     return (
         <Container> 
